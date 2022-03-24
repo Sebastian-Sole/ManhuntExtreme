849e7cfc526b2a83e17f3234e526bd27914742ed
Sebastian Sole <sebastrs@stud.ntnu.no>
2022-03-24 21:24:14 +0100

    Game engine in PluginMain

--- src/main/java/manhunt_extreme/PluginMain.java
+++ src/main/java/manhunt_extreme/PluginMain.java
@@ -1,18 +1,22 @@
 package manhunt_extreme;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.logging.Logger;
 
 
 public final class PluginMain extends JavaPlugin {
 
     Logger logger = Logger.getLogger("manhunt_extreme.PluginMain");
+    GameEngine gameEngine;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        gameEngine = new GameEngine();
         logger.info("Plugin Enabled!");
     }
+
 
     @Override
     public void onDisable() {
