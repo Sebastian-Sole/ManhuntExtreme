6af4e8b50e4a2ce957c4289aac0c2ca93f9a55a0
Sebastian Sole <sebastrs@stud.ntnu.no>
2022-03-25 16:30:07 +0100

    feat: Task manager class

--- src/main/java/manhunt_extreme/GameEngine.java
+++ src/main/java/manhunt_extreme/GameEngine.java
@@ -1,12 +1,30 @@
 package manhunt_extreme;
 
+import manhunt_extreme.task_manager.TaskManager;
+
 public class GameEngine {
 
     private Game game = new Game(false);
+
+    private TaskManager taskManager = new TaskManager();
 
     public GameEngine() {
 
     }
 
+    public Game getGame() {
+        return game;
+    }
 
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public TaskManager getTaskManager() {
+        return taskManager;
+    }
+
+    public void setTaskManager(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
 }
---
+++ src/main/java/manhunt_extreme/task_manager/TaskManager.java
@@ -0,0 +1,14 @@
+package manhunt_extreme.task_manager;
+
+public class TaskManager {
+
+    //Todo: Compass object
+    //Todo: BlazeSpawner object
+    //Todo: Actionbar object
+    //Todo: Supply drop object
+    //Todo: Haste object
+
+    public TaskManager() {
+        //Todo:
+    }
+}
