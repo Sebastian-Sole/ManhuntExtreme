1edffc1f5740dd26d250b1dda89b7ccf9ab5f661
Sebastian Sole <sebastrs@stud.ntnu.no>
2022-03-30 13:53:44 +0200

    feat: GameBalancingCalculator

--- src/main/java/manhunt_extreme/calculators/GameBalancingCalculator.java
+++ src/main/java/manhunt_extreme/calculators/GameBalancingCalculator.java
@@ -7,20 +7,45 @@ import manhunt_extreme.manhunt_team.ManhuntTeam;
 public class GameBalancingCalculator {
 
     private final GameEngine gameEngine;
+    private Double hunterTeamScore;
+    private Double runnerTeamScore;
 
     public GameBalancingCalculator(GameEngine gameEngine) {
         this.gameEngine = gameEngine;
+        this.hunterTeamScore = getTeamScore(gameEngine.getHunters());
+        this.runnerTeamScore = getTeamScore(gameEngine.getRunners());
     }
 
-    public double calculatePlayerScore(ManhuntPlayer manhuntPlayer) {
-        PlayerScoreCalculator playerScoreCalculator = new PlayerScoreCalculator(manhuntPlayer);
-//        return playerScoreCalculator.getScore();
-        return 0;
+    //Todo: Average Score
+    public Double getAverageOfAllTeamScores() {
+        return (hunterTeamScore * runnerTeamScore) / 2;
     }
 
-    public double calculateTeamScore(ManhuntTeam manhuntTeam) {
-        TeamScoreCalculator teamScoreCalculator = new TeamScoreCalculator(manhuntTeam);
-        return 0;
+    //Todo: Team Score
+    public Double getTeamScore(ManhuntTeam manhuntTeam) {
+        return manhuntTeam.getPlayerList().stream().mapToDouble(ManhuntPlayer::getPlayerScore).sum();
+    }
+
+    //Todo: Team score difference
+    public Double getTeamScoreDifference() {
+        return runnerTeamScore - hunterTeamScore;
+    }
+
+    //Todo: Individual player score difference
+//    public Double playerDifference(ManhuntPlayer manhuntPlayer) {
+//        if (manhuntPlayer.getTeam() instanceof HunterTeam) {
+//            return manhuntPlayer.getPlayerScore()
+//                    - (runnerTeamScore / gameEngine.getRunners().getPlayerList().size());
+//        } else if (manhuntPlayer.getTeam() instanceof RunnerTeam) {
+//
+//        } else {
+//            throw new IllegalArgumentException("Manhunt player has no team. ManhuntTeam: " + manhuntPlayer.getTeam().toString());
+//        }
+//    }
+    public Double getPlayerScoreDifference() {
+        Double runnerTeamAverageScore = runnerTeamScore / gameEngine.getRunners().getPlayerList().size();
+        Double hunterTeamAverageScore = hunterTeamScore / gameEngine.getHunters().getPlayerList().size();
+        return runnerTeamAverageScore - hunterTeamAverageScore;
     }
 
 }
