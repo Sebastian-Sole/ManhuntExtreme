9a8bef082fca784fc284db3e9f147b107dc72985
Sebastian Sole <sebastrs@stud.ntnu.no>
2022-04-07 18:01:52 +0200

    feat: BlazeSpawnerHandler

--- src/main/java/manhunt_extreme/task_manager/BlazeSpawnerHandler.java
+++ src/main/java/manhunt_extreme/task_manager/BlazeSpawnerHandler.java
@@ -1,9 +1,90 @@
 package manhunt_extreme.task_manager;
 
 import manhunt_extreme.PluginMain;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+
+import java.util.Random;
 
 public class BlazeSpawnerHandler {
+    private Random random = new Random();
+    private PluginMain pluginMain;
+    private int spawnersGenerated;
+
     public BlazeSpawnerHandler(PluginMain pluginMain) {
+        this.pluginMain = pluginMain;
+    }
+
+
+    public void start() {
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(pluginMain, () -> {
+            if (spawnersGenerated < 5) {
+                generateSpawner();
+                spawnersGenerated++;
+            }
+        }, 8400L, 3600L);
+    }
+
+    private void generateSpawner() {
+        var nether = Bukkit.getWorld("world_nether");
+        var x = generateCoordinate();
+        var y = random.nextDouble(54.0) + 36;
+        var z = generateCoordinate();
+        var firstBlock = new Location(nether, x, y, z);
+        var block = nether.getBlockAt(firstBlock);
+        block.setType(Material.SPAWNER);
+        var blockState = (CreatureSpawner) block.getState();
+        blockState.setSpawnedType(EntityType.BLAZE);
+        generatePlatform(block, nether);
+        Bukkit.broadcastMessage("Blaze spawner generated at: " + (int) x + ", " + (int) y + ", " + (int) z);
+    }
+
+    private void generatePlatform(Block block, World nether) {
+        var spawnBlockLocation = block.getLocation();
+        var locX = (int) spawnBlockLocation.getX();
+        var locY = (int) spawnBlockLocation.getY();
+        var locZ = (int) spawnBlockLocation.getZ();
+
+        // Make 4 quadrants around the block
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                nether.getBlockAt(locX + i, locY, locZ + j).setType(Material.OBSIDIAN);
+            }
+        }
+
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                nether.getBlockAt(locX - i, locY, locZ + j).setType(Material.OBSIDIAN);
+            }
+        }
+
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                nether.getBlockAt(locX - i, locY, locZ - j).setType(Material.OBSIDIAN);
+            }
+        }
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                nether.getBlockAt(locX + i, locY, locZ - j).setType(Material.OBSIDIAN);
+            }
+        }
 
     }
+
+
+    private double generateCoordinate() {
+        var axis = 1;
+        if (Math.random() < 0.5) {
+            axis = -1;
+        }
+        return random.nextDouble(300.0) * axis;
+    }
+
+
 }
